09943f87f373afc4a3c6651356b7bb0a6d19241c
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2018-05-25 20:00:48 +0530

    Add code for letting computer take chances

--- script.js
+++ script.js
@@ -30,23 +30,34 @@ function startGame() {
 // check if a cell has been clicked by a human
 function humanClicked(event)
 {
-  gameAdvance(event.target.id,human);
-  origBoard[event.target.id]=human;
-  document.getElementById(event.target.id).innerText = human;
-  let gameStatus = checkWin(origBoard,human);
+  if( origBoard[event.target.id] !== 'O' || origBoard[event.target.id] !== 'X' )
+    {
+      var status = gameAdvance(event.target.id,human);
+      if(status==0 && !checkTie())
+      {
+        gameAdvance(bestSpot(),computer);
+      }
+    }
 }
 
 // will check the game status also after every move
 function gameAdvance(currCell,player)
 {
   origBoard[currCell]=player;
-  document.getElementById(cell).innerText = player;
+  document.getElementById(currCell).innerText = player;
 
   //  checkif after the current move of player, has he won
   let gameStatus = checkWin(origBoard,player);
-
+  if(gameStatus)
+  {
+    gameOver(gameStatus);
+    return 1;
+  }
+  console.log("hi");
+  return 0;
 }
 
+// to check if the player has won the game with the given board situation
 function checkWin(board,player)
 {
   var temp=new Array(9).fill(0);;
@@ -54,7 +65,7 @@ function checkWin(board,player)
   //  storing where all the player has moved
   for(var i=0;i<9;i++)
   {
-    if(board[i]==player)
+    if(board[i]===player)
     {
       temp[i]=1;
     }
@@ -63,7 +74,7 @@ function checkWin(board,player)
   // comparing with the winning combos
   flag=0;
   let gameWon = null;
-  for(var i=0;i<9;i++)
+  for(var i=0;i<8;i++)
   {
     for(var j=0;j<3;j++)
     {
@@ -71,9 +82,9 @@ function checkWin(board,player)
       {
         break;
       }
-      else if(j===2)
+      else if(temp[winCombos[i][j]]===1 && j===2)
       {
-          flag=1
+          flag=1;
       }
     }
 
@@ -83,10 +94,60 @@ function checkWin(board,player)
       gameWon = {};
       gameWon['indexOfWinComb']=i;
       gameWon['player']=player;
+      break;
     }
   }
 
   //  return the winning details object
   return gameWon;
 
+}
+
+// doing necessary things after the game is over
+function gameOver(gameWon)
+{
+  // changing colour of current winning combination
+  for (let index of winCombos[gameWon['indexOfWinComb']])
+  {
+		document.getElementById(index).style.backgroundColor = gameWon['player'] === human ? "blue" : "red";
+	}
+
+  // removing all event listeners
+  for (var i = 0; i < cells.length; i++)
+  {
+		cells[i].removeEventListener('click', humanClicked, false);
+	}
+
+  // declaring winner
+  declareWinner(gameWon.player == human ? "You win!" : "You lose.");
+}
+
+// function to show the text after gameOver
+function declareWinner(textShow) {
+	document.querySelector(".endgame").style.display = "block";
+	document.querySelector(".endgame .text").innerText = textShow;
+}
+
+// function to return the array with cells remaining
+function emptySquares() {
+	return origBoard.filter(s => typeof s == 'number');
+}
+
+// finding the best spot for the computer to play
+function bestSpot() {
+	return emptySquares()[0];
+}
+
+//  function to test if the game has been tied
+function checkTie()
+{
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "green";
+			cells[i].removeEventListener('click', humanClicked, false);
+		}
+		declareWinner("Tie Game!")
+		return true;
+	}
+	return false;
 }
